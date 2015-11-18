d2de4d0c51950067d54f534dacabbe5ed79b2e67
Dan Buch <dan@travis-ci.org>
2015-11-17 23:27:26 -0500

    Switch to thor options

--- lib/system_info/cli.rb
+++ lib/system_info/cli.rb
@@ -13,82 +13,78 @@ include Term::ANSIColor
 
 module SystemInfo
   class Cli < Thor
+    option :cookbooks_sha, type: :string, aliases: '-S',
+           desc: 'SHA1 of the travis-cookbooks tree at master, e.g. "a1b2c3d"',
+           default: ENV['COOKBOOKS_SHA']
+    option :formats, type: :string, aliases: '-F',
+           desc: 'Output format(s), e.g. "human,json" or "human"',
+           default: (ENV['FORMATS'] || 'human').split(/\s*,\s+/)
+    option :human_output, type: :string, aliases: '-H',
+           desc: 'Output file of the human format (default $stdout)',
+           default: ENV['HUMAN_OUTPUT']
+    option :json_output, type: :string, aliases: '-J',
+           desc: 'Output file of the json format (default $stdout, after human)'
+    option :commands_file, type: :string, aliases: '-f',
+           desc: 'YAML commands file to run',
+           default: (
+             ENV['COMMANDS_FILE'] ||
+             File.expand_path('../commands.yml', __FILE__)
+           )
     desc 'report', 'runs a Travis-style system info scan/report'
+    long_desc <<-LONGDESC
+      Gather and report a bunch of system information specific to the
+      travis build environment.
+    LONGDESC
     def report
-      if ARGV.first =~ /-h|--help|help/
-        puts <<-EOF.gsub(/^\s+>/, '')
-        >Usage: #{File.basename($PROGRAM_NAME)}
-        >
-        >Gather and report a bunch of system information specific to the
-        >travis build environment.
-        >
-        >Influential environment variables:
-        >
-        >  COOKBOOKS_SHA - SHA1 of the travis-cookbooks tree at master, e.g. "a1b2c3d"
-        >  FORMATS       - Output format(s), e.g. "human,json" or "human"
-        >  HUMAN_OUTPUT  - Output file of the human format (default $stdout)
-        >  JSON_OUTPUT   - Output file of the json format (default $stdout, after human)
-        >  COMMANDS_FILE - YAML commands file to run (default commands.yml)
-        >
-        EOF
-        exit 0
-      end
-
-      ENV['FORMATS'] ||= 'human'
-
-      host_os = case RbConfig::CONFIG['host_os']
-                when /^darwin/
-                  'osx'
-                when /^linux/
-                  'linux'
-                else
-                  fail "Unknown host OS: #{RbConfig::CONFIG['host_os']}"
-                end
-
       @system_info = { system_info: {} }
 
-      @human_stdout = ENV['HUMAN_OUTPUT'] ? File.open(ENV['HUMAN_OUTPUT'], 'w') : $stdout
-      @json_stdout = ENV['JSON_OUTPUT'] ? File.open(ENV['JSON_OUTPUT'], 'w') : $stdout
+      @human_stdout = options[:human_output] ? File.open(options[:human_output], 'w') : $stdout
+      @json_stdout = options[:json_output] ? File.open(options[:json_output], 'w') : $stdout
 
-      commands = YAML.load_file(
-        ENV['COMMANDS_FILE'] || File.expand_path('../system_info/commands.yml', __FILE__)
-      )['commands']
+      commands = YAML.load_file(options[:commands_file])['commands']
 
-      @cookbooks_sha = (/\A(?<sha>[0-9a-f]{7})\z/.match(cookbooks_sha) || {})[:sha]
+      @cookbooks_sha = (/\A(?<sha>[0-9a-f]{7})\z/.match(options[:cookbooks_sha]) || {})[:sha]
 
       at_exit do
-        if ENV['FORMATS'] =~ /\bjson\b/ && !@system_info[:system_info].empty?
+        if formats.include?('json') && !@system_info[:system_info].empty?
           require 'json'
           @json_stdout.puts JSON.pretty_generate(@system_info)
         end
       end
 
-      (Array(commands[host_os]) + [print_cookbooks_sha] + Array(commands['common'])).compact.each do |cmd|
+      all_commands = (
+        Array(commands[host_os]) + [print_cookbooks_sha] + Array(commands['common'])
+      )
+
+      all_commands.compact.each do |cmd|
         begin
-          if cmd.is_a? Hash
+          if cmd.is_a?(Hash)
             command = Array(cmd['command'])
-            name    = cmd['name']
-            pipe    = " | #{cmd['pipe']}" if cmd['pipe']
-            pre     = cmd['pre']
-            post    = cmd['post']
-            port    = cmd['port']
+            name = cmd['name']
+            pipe = " | #{cmd['pipe']}" if cmd['pipe']
+            pre = cmd['pre']
+            post = cmd['post']
+            port = cmd['port']
           else
             command = Array(cmd)
           end
 
-          pre && `#{pre}`
+          system(pre, [:out, :err] => '/dev/null') if pre
 
-          wait_for port if port
+          wait_for(port) if port
 
-          invoke = ['bash', '-l', '-c', command.map(&:shellescape), pipe].compact.flatten.join ' '
+          invoke = [
+            'bash', '-l', '-c', command.map(&:shellescape), pipe
+          ].compact.flatten.join(' ')
+
           output = `#{invoke} 2>/dev/null`.chomp
 
-          post && `#{post}`
+          system(post, [:out, :err] => '/dev/null') if post
 
           if output.length > 0
             job = { name: name, command: command, output: output }
-            output_human_readable(job) if ENV['FORMATS'] =~ /\bhuman\b/
-            output_json(job) if ENV['FORMATS'] =~ /\bjson\b/
+            output_human_readable(job) if formats.include?('human')
+            output_json(job) if formats.include?('json')
           end
         rescue Errno::ENOENT
         rescue => e
@@ -103,6 +99,25 @@ module SystemInfo
 
     private
 
+    def formats
+      @formats ||= Array(
+        options[:formats]
+      ).map { |s| s.split(/,/) }.reject(&:empty?).flatten.compact
+    end
+
+    def host_os
+      return @host_os if @host_os
+
+      case RbConfig::CONFIG['host_os']
+      when /^darwin/
+        @host_os = 'osx'
+      when /^linux/
+        @host_os = 'linux'
+      else
+        fail "Unknown host OS: #{RbConfig::CONFIG['host_os']}"
+      end
+    end
+
     def cookbooks_sha
       ENV['COOKBOOKS_SHA']
     end
@@ -113,8 +128,14 @@ module SystemInfo
 
     def output_human_readable(job)
       if defined?(Term) && defined?(Term::ANSIColor)
-        @human_stdout.print blue, bold, (job[:name] || Array(job[:command]).join(' ')), reset, "\n"
-        @human_stdout.print job[:output], "\n"
+        @human_stdout.print(
+          blue,
+          bold,
+          (job[:name] || Array(job[:command]).join(' ')),
+          reset,
+          "\n"
+        )
+        @human_stdout.print(job[:output], "\n")
         return
       end
 
@@ -128,7 +149,16 @@ module SystemInfo
       info = { output: out }
       info[:name] = job[:name] if job[:name]
       info[:name] ||= job[:command]
-      key = job[:key] || [*info[:name]].first.downcase.gsub(/[^a-zA-z0-9]/, '_').gsub(/_+/, '_').sub(/_*$/, '')
+      key = (
+        job[:key] ||
+        [*info[:name]].first.downcase.gsub(
+          /[^a-zA-z0-9]/, '_'
+        ).gsub(
+          /_+/, '_'
+        ).sub(
+          /_*$/, ''
+        )
+      )
       @system_info[:system_info][key] = info
     end
 
@@ -154,7 +184,8 @@ module SystemInfo
 
     def print_cookbooks_sha
       {
-        'command' => "echo #{@cookbooks_sha} '#{cookbooks_commit_url_template % @cookbooks_sha}'",
+        'command' => "echo #{@cookbooks_sha} " \
+                     "'#{cookbooks_commit_url_template % @cookbooks_sha}'",
         'name' => 'Cookbooks Version'
       } if @cookbooks_sha
     end
